3c905fafd307c6769ee1136a757d8c777d2296de
Israel Heiblum <Israel_Heiblum@amat.com>
2020-09-12 15:01:33 +0300

    backend workspace ready

--- src/main/java/cal_hackathon/Controller.java
+++ src/main/java/cal_hackathon/Controller.java
@@ -1,11 +1,16 @@
 package cal_hackathon;
 
 import cal_hackathon.business_logic.Application;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Israel Heiblum
+ * @author CAL-GROUP
  */
 
 @RestController
@@ -14,5 +19,19 @@ public class Controller {
     @RequestMapping("/")
     public String index() {
         return this.app.appActions();
+    }
+
+    @CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*")
+    @RequestMapping(value = "countPlus" +
+                            "/{num}", method = RequestMethod.GET, produces = MediaType.ALL_VALUE)
+    public  int countPlus(@PathVariable int num) {
+       return app.addOne(num);
+    }
+
+    @CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*")
+    @RequestMapping(value = "countMinus" +
+                            "/{num}", method = RequestMethod.GET, produces = MediaType.ALL_VALUE)
+    public  int countAdd(@PathVariable int num) {
+        return app.subtractOne(num);
     }
 }
--- src/main/java/cal_hackathon/business_logic/Application.java
+++ src/main/java/cal_hackathon/business_logic/Application.java
@@ -1,12 +1,21 @@
 package cal_hackathon.business_logic;
 
 /**
- * @author Israel Heiblum
+ * @author CAL-GROUP
  */
 
 public class Application {
 
     public String appActions () {
         return "We are the champions my friend!!!";
+    }
+
+    public int addOne(int num) {
+        return num + 1;
+    }
+
+
+    public int subtractOne(int num) {
+        return num - 1;
     }
 }
